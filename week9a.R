6f31d52c6690acfc0c1e2085d35bc129dd758a9b
kira2228 <157898603+kira2228@users.noreply.github.com>
2024-11-29 11:16:50 +0530

    Create week9a.R

---
+++ week9a.R
@@ -0,0 +1,17 @@
+x <- c(141,134,178,156,108,116,119,143,162,130)
+y <- c(62,85,56,21,47,17,76,92,62,58)
+relationship_model <- lm(y~x)
+print(relationship_model)
+
+x <- c(141,134,178,156,108,116,119,143,162,130)
+y <- c(62,85,56,21,47,17,76,92,62,58)
+relationship_model <- lm(y~x)
+print(summary(relationship_model))
+
+x <- c(141,134,178,156,108,116,119,143,162,130)
+y <- c(62,85,56,21,47,17,76,92,62,58)
+relationship_model <- lm(y~x)
+png(file= "linear_regression.png")
+plot(y,x,col="red",main="Height and weight regression", abline(lm(x~y)),cex=1.3,pch=16,xlab="height in kg",ylab="height in cm")
+dev.off()
+
